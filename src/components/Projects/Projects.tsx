32ed9aa8b75d706881417105718c03afc0c915d7
Martin Bruno <martinbruno30@gmail.com>
2023-12-06 19:37:50 -0300

    Update Projects.tsx

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+
+import { useEffect } from "react";
 import ProjectCard from "../ProjectCard/ProjectCard";
 
 interface Project {
